f9f8d7ff6c71360eb343d59349e9908393e7d68e
wglee0511 <ranazang@naver.com>
2021-06-11 15:18:19 +0900

    find_max_num

---
+++ app.py
@@ -0,0 +1,15 @@
+input = [3, 5, 6, 1, 2, 4]
+
+
+
+
+def find_max_num(array):
+    tem_num = 0
+    for each in array :
+        if each > tem_num :
+            tem_num = each
+    return tem_num
+
+
+result = find_max_num(input)
+print(result)
